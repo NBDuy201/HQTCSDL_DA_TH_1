33ffbe51c1c2142a168fc8c1ed0b4eee40bd9051
mon gruvia <thaonhucute2001@gmail.com>
2021-11-12 00:47:52 +0700

    'update'

--- ProcedureList_KhachHang.sql
+++ ProcedureList_KhachHang.sql
@@ -157,6 +157,14 @@ BEGIN
 			RETURN
 		END
 
+	-- Đơn hàng đã được khách hàng đồng ý trước đó
+	IF EXISTS ( SELECT TINHTRANGDONHANG FROM DONHANG DH WHERE DH.MADONHANG = @MaDonHang AND DH.TINHTRANGDONHANG <> N'Chưa đồng ý')
+		BEGIN
+			RAISERROR (N'Không thể thêm sản phẩm vào đơn hàng đã đi giao.', -1, -1)
+			ROLLBACK TRAN
+			RETURN
+		END
+
 	-- Số lượng thêm ít hơn số lượng tồn của chi nhánh đang chọn
 	IF @SoLuong > ( SELECT CN_SP.SOLUONGTON
 					FROM SANPHAM SP, CHINHANH_SANPHAM CN_SP 
@@ -203,3 +211,41 @@ BEGIN
 END
 
 GO
+
+-- =============================================
+--- Đồng ý đơn hàng:
+--- Input: Mã Đơn hàng ở trạng thái chưa đồng ý
+--- Output: Cập nhật tình trạng đơn hàng = đồng ý
+-- =============================================
+CREATE OR ALTER PROCEDURE DongY_DonHang
+	@MaDonHang char(10)
+
+AS
+BEGIN
+	BEGIN TRAN
+	-- Đơn hàng không ở tình trạng chưa đồng ý
+	IF NOT EXISTS ( SELECT TINHTRANGDONHANG FROM DONHANG DH WHERE DH.MADONHANG = @MaDonHang AND DH.TINHTRANGDONHANG <> N'Chưa đồng ý')
+		BEGIN
+			RAISERROR (N'Đơn này đã được đồng ý rồi.', -1, -1)
+			ROLLBACK TRAN
+			RETURN
+		END
+
+	-- Đơn hàng chưa có sản phẩm nào
+	IF NOT EXISTS ( SELECT CT.MADONHANG
+					FROM DONHANG DH, CHITIETDONHANG_SANPHAM CT 
+					WHERE CT.MADONHANG = DH.MADONHANG )
+		BEGIN
+			RAISERROR (N'Đơn hàng chưa có sản phẩm nào.', -1, -1)
+			ROLLBACK TRAN
+			RETURN
+		END
+
+	-- Cập nhật tình trạng đơn hàng
+	UPDATE DONHANG 
+	SET TINHTRANGDONHANG = N'Đồng ý'
+	WHERE MADONHANG = @MaDonHang
+	
+	COMMIT TRAN
+END
+
