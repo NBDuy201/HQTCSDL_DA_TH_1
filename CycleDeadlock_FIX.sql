732e4dea46dd7f4e36903e3b03675d5a86733fab
THƯƠNG NGUYỄN HOÀI <19127287@student.hcmus.edu.vn>
2021-11-20 10:00:20 +0700

    2 Deadlock

---
+++ CycleDeadlock_FIX.sql
@@ -0,0 +1,66 @@
+﻿
+GO
+USE [QLDatChuyenHangOnl]
+GO
+
+-- =============================================
+-- DongY_DonHang_FIX là procedure giống hệt DongY_DonHang 
+-- Thứ tự cập nhật của bảng HOADON và CHINHANH_SANPHAM cũng được giữ nguyên
+-- Cụ thể: 1.DONHANG --> 2.CHINHANH_SANPHAM
+-- =============================================
+
+CREATE OR ALTER PROCEDURE DongY_DonHang_FIX
+	@MaDonHang bigint
+
+AS
+BEGIN
+	BEGIN TRAN
+	-- Đơn hàng không ở tình trạng chưa đồng ý
+	IF EXISTS ( SELECT TINHTRANGDONHANG FROM DONHANG DH WHERE DH.MADONHANG = @MaDonHang AND DH.TINHTRANGDONHANG <> N'Chưa đồng ý')
+		BEGIN
+			RAISERROR (N'Đơn này đã được đồng ý rồi.', -1, -1)
+			ROLLBACK TRAN
+			RETURN
+		END
+
+	-- Đơn hàng chưa có sản phẩm nào
+	IF NOT EXISTS ( SELECT CT.MADONHANG
+					FROM DONHANG DH, CHITIETDONHANG_SANPHAM CT 
+					WHERE CT.MADONHANG = DH.MADONHANG )
+		BEGIN
+			RAISERROR (N'Đơn hàng chưa có sản phẩm nào.', -1, -1)
+			ROLLBACK TRAN
+			RETURN
+		END
+
+	-- Cập nhật tình trạng đơn hàng
+	UPDATE DONHANG 
+	SET TINHTRANGDONHANG = N'Đồng ý'
+	WHERE MADONHANG = @MaDonHang
+
+	-- Cập nhật lại số lượng tồn các sản phẩm của chi nhánh
+
+	WAITFOR DELAY '00:00:10'
+
+	UPDATE CHINHANH_SANPHAM
+	SET SOLUONGTON = SOLUONGTON - DH_SP.SOLUONGTUONGUNG
+
+	FROM CHINHANH_SANPHAM CN_SP
+	INNER JOIN CHITIETDONHANG_SANPHAM DH_SP
+	ON CN_SP.MASANPHAM = DH_SP.MASANPHAM AND
+		DH_SP.MADONHANG = @MaDonHang
+		
+	IF EXISTS ( SELECT SOLUONGTON
+				FROM CHINHANH_SANPHAM
+				WHERE SOLUONGTON < 0 )
+
+	BEGIN
+		RAISERROR (N'Số lượng đang đặt vượt quá số lượng tồn của chi nhánh.', -1, -1)
+		ROLLBACK TRAN
+		RETURN
+	END
+
+	COMMIT TRAN
+END
+
+GO
